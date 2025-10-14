229876ab666303257ef3e55a7ed009a8c0398f7c
Graciano Marôto <gracianoml07@gmail.com>
2025-10-14 18:17:41 -0300

    add logo

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,13 +1,12 @@
 <template>
 <q-page class="flex flex-center">
 
-  <div class="q-gutter-sm">
+  <div class="q-gutter-md">
     
     <div class="text-center">
-      <q-img src="/icons/Logomarca-AurumDesk.png" height="140px" width="140px"/>
+        <q-img src="/icons/Logomarca-AurumDesk.png" height="140px" width="145px"/>
+        <q-img src="/icons/Logo-AA-SVG.svg" height="115px" width="167px"/>
     </div>
-    <h4 class="text-center">LOGOMARCA<br>ÁLVARO ALIANÇAS</h4>
-
     <div class="text-center">
       <div class="q-gutter-sm">  
         <q-btn size="22px" style="background: goldenrod; color: white; width: 315px;" label="+ Nova Venda" to="/registrarvendas" />
