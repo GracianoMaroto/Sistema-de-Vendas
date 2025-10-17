fa76f0303de0949a85e07850cabc99c59350f41c
Graciano Maroto <gracianoml07@gmail.com>
2025-10-17 11:17:05 -0300

    add footer no login

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -2,16 +2,14 @@
 <q-page class="flex flex-center">
 
   <div class="q-gutter-md">
-    
-    <div class="text-center">
-        <q-img src="/icons/Logomarca-AurumDesk.png" height="140px" width="145px"/>
-        <q-img src="/icons/Logo-AA-SVG.svg" height="115px" width="167px"/>
+    <div class="q-gutter-lg text-center">
+      <q-img src="/icons/Logomarca-AurumDesk.png" height="110px" width="115px" style="text-align: center;"/>
+      <q-img src="/icons/Logo-AA-SVG.svg" height="75px" width="110px" style="text-align: center;"/>
     </div>
-    <div class="text-center">
-      <div class="q-gutter-sm">  
+
+      <div class="q-gutter-sm text-center">
         <q-btn size="22px" style="background: goldenrod; color: white; width: 315px;" label="+ Nova Venda" to="/registrarvendas" />
         <q-btn size="22px" style="background: goldenrod; color: white; width: 315px;" label="+ Novo Serviço" to="/registrarservicos" />
-      </div>  
     </div>
   </div>
 
